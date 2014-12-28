3b8de6835ab6d84495258a4ce9f8f120b122b0b4
Tomo Hung <tomohung@gmail.com>
2014-12-28 21:25:01 +0800

    finish basic tic-tac-toe, smarter computer choose is waiting to do

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,11 +8,20 @@
 
 require 'pry'
 
+THREE_LINE_SET = [[1, 2, 3],[4, 5, 6],[7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+COMPUTER_PICK = -1
+USER_PICK = 1
+TIE_GAME = 0
+
+def say(message)
+  puts "=> #{message}"
+end
+
 def output(hash_value)
   case hash_value
-  when 0
+  when USER_PICK
     'x'
-  when 1
+  when COMPUTER_PICK
     'o'
   else
     ' '
@@ -44,58 +53,83 @@ end
 def user_pick_square(input_hash)
   
   begin
-    puts "Choose one square (1 ~ 9): "
+    say "Choose one square (1 ~ 9): "
     input = gets.chomp
     result = input =~ /[1-9]/
 
     if !result
-      puts "Please input 1 ~ 9"
+      say "Please input 1 ~ 9"
       redo
     end
 
     if input_hash[input.to_i]
-      puts "Has Choosen!"
+      say "#{input} Has Choosen!"
       redo
     end
 
-    input_hash[input.to_i] = 0
+    input_hash[input.to_i] = USER_PICK
   end until input_hash[input.to_i]
 end
 
 def computer_pick_square(input_hash)
   (1..9).to_a.each do |x|
     if input_hash[x] == nil
-      input_hash[x] = 1
+      input_hash[x] = COMPUTER_PICK
       break
     end
   end 
 end
 
 def game_is_over?(input_hash)
-  unchoosen_index = (1..9).to_a.select {|x| input_hash[x].nil? }
-  if unchoosen_index.count == 0
-    puts "It's tie..."
-    return true
-  end 
+  unchoosen_array = (1..9).to_a.select {|x| input_hash[x].nil? }
+  return TIE_GAME if unchoosen_array.empty?
+
+  result = nil
+  THREE_LINE_SET.each do |set|
+    
+    result = 0
+    set.each do |x|
+      if input_hash.has_key?(x)
+        result += input_hash[x]
+      end
+      return USER_PICK if result == 3 
+      return COMPUTER_PICK if result == -3
+    end
+  end
 
   return false
 
 end
 
-
 begin
   squares_hash = {}
-  loop do
+  begin
     system("clear")
-    puts "Welcome to Tomo's Tic-Tac-Toe Game" 
+    say "Welcome to Tomo's Tic-Tac-Toe Game" 
     show_tic_tac_toe_figure(squares_hash)
     
     user_pick_square(squares_hash)
+    check_status = game_is_over?(squares_hash)
+    break if check_status
+
     computer_pick_square(squares_hash)
+    check_status = game_is_over?(squares_hash)
+  end until check_status
 
-  end until game_is_over?(squares_hash)
+  system("clear")
+  say "Result: "
+  show_tic_tac_toe_figure(squares_hash)
+  case check_status
+  when TIE_GAME
+    say "It's tie"
+  when USER_PICK
+    say "You Win!!"
+  when COMPUTER_PICK
+    say "Computer Win!!"
+  end  
 
-  puts "Play again? (y/n)"
+  puts "\n"
+  say "Play again? (y/n)"
 end while gets.chomp.downcase == 'y'
 
-puts "Exit Tic-Tac-Toe Game"
+say "Exit Tic-Tac-Toe Game"
