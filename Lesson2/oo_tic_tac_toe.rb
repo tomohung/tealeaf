d56d9b8f4fe8570260203dc0a0181553819fa549
Tomo Hung <tomohung@gmail.com>
2015-01-03 23:25:47 +0800

    add AI for tic tac toe

--- Lesson2/oo_tic_tac_toe.rb
+++ Lesson2/oo_tic_tac_toe.rb
@@ -14,8 +14,8 @@ class Player
   attr_accessor :name, :picked_numbers
 
   def initialize(name)
-    @name = name
-    @picked_numbers = []
+    self.name = name
+    self.picked_numbers = []
   end
 
   def get_straight_number?
@@ -31,18 +31,97 @@ class Player
     return false
   end
 
-  def pick
-    gets.chomp.to_i
-  end
-
-  def add(number)
-    self.picked_numbers << number
+  def pick!(valid_numbers)
+    value = 0
+    loop do
+      value = gets.chomp.to_i
+      break if valid_numbers.include?(value)
+      puts "Input number is invalid, try again..."
+    end
+    valid_numbers.delete(value)
+    self.picked_numbers << value
   end
 
 end
 
 class Robot_Player < Player
 
+  def pick!(valid_numbers)
+    choose_number = nil
+
+    # 1. pick number to let computer get a straight line
+    choose_number ||= pick_number_to_win(valid_numbers)
+    # 2. pick number to prevent user get a straight line
+    choose_number ||= pick_number_to_prevent_lose(valid_numbers)
+    # 3. pick number to get a maximum possible win
+    choose_number ||= pick_number_to_possible_win(valid_numbers)
+    
+    return unless choose_number    
+    valid_numbers.delete(choose_number)
+    self.picked_numbers << choose_number
+  end 
+   
+  def test_straight_line?(test_picked_numbers, test_number)
+    TicTacToeRuler::THREE_LINE_SET.each do |set| 
+      if !set.include?(test_number)
+        next
+      end
+      line = set.select do |number| 
+        number == test_number || test_picked_numbers.include?(number)
+      end
+      return true if line.size == set.size
+    end
+    false
+  end
+
+  def pick_number_to_win(valid_numbers)
+    valid_numbers.each do |number|
+      return number if test_straight_line?(picked_numbers, number)
+    end
+    return nil #if there is no any match
+  end
+
+  def pick_number_to_prevent_lose(valid_numbers)
+    opposite_player_picked_number = 
+      TicTacToeRuler::PICK_NUMBERS.select |number| 
+        !picked_numbers.include?(number) &&
+        !valid_numbers.include?(number)
+      end
+    valid_numbers.each do |number|
+      return number if test_straight_line?(opposite_player_picked_number, number)
+    end
+    return nil
+  end
+
+  def retrieve_possible_winning_number(result_hash)
+    last_result = 0
+    choose_number = 0
+    result_hash.each do |key, value|
+      if value > last_result
+        last_result = value
+        choose_number = key
+      end
+    end
+    return choose_number
+  end  
+
+  def pick_number_to_possible_win(valid_numbers)
+    result_hash = {}
+    valid_numbers.each do |number|
+      result = 0     
+      TicTacToeRuler::THREE_LINE_SET.each do |set|      
+        set.each do |element|
+          if !valid_numbers.include?(element) && !picked_numbers.include?(element)
+            result -= 1
+          else
+            result += 1
+          end
+        end
+      end
+      result_hash[number] = result
+    end 
+    return retrieve_possible_win_number(result_hash)
+  end
 end
 
 class TicTacToeBoard
@@ -51,35 +130,33 @@ class TicTacToeBoard
 
 private
   def initialize(player1, player2)
-    self.unpicked_numbers = TicTacToeRuler::PICK_NUMBERS
+    self.unpicked_numbers = TicTacToeRuler::PICK_NUMBERS.clone
     self.user1 = player1
     self.user2 = player2
     self.current_user = player1
   end
   
   def output_who_picked(value)
-    case value
-    when user1.picked_number.include?(value)
-      'x'
-    when user2.picked_number.include?(value)
-      'o'
+    word = ""
+    case 
+    when user1.picked_numbers.include?(value)
+      word = 'x'
+    when user2.picked_numbers.include?(value)
+      word = 'o'
     else
-      ' '
+      word = ' '
     end
+    word
   end  
 
 public
 
   def ask_player_to_pick
-    number = self.current_user.pick
-    return false if !unpicked_numbers.include?(number)
-    unpicked_numbers.delete(number)
-    self.current_user.add(number)
+    self.current_user.pick!(unpicked_numbers)
     self.current_user = self.current_user == user1 ? user2 : user1
   end
 
   def show_figure
-
     if unpicked_numbers.size == TicTacToeRuler::PICK_NUMBERS.size
       puts "
        1 | 2 | 3
@@ -90,7 +167,6 @@ public
       "
       return
     end
-
     puts "
        #{output_who_picked(1)} | #{output_who_picked(2)} | #{output_who_picked(3)}  
       ---+---+---
@@ -107,6 +183,11 @@ public
     return false
   end
 
+  def clear
+    self.unpicked_numbers = TicTacToeRuler::PICK_NUMBERS.clone
+    user1.picked_numbers.clear
+    user2.picked_numbers.clear
+  end  
 end
 
 class TicTacToeGame
@@ -142,19 +223,19 @@ class TicTacToeGame
   def play
 
     loop do
+      result = ""
+      game_board.clear
+
       loop do
         system("clear")
         say "Welcome to Tomo's Tic-Tac-Toe Game" 
         game_board.show_figure
         
-        say "-- #{game_board.current_user.name}'s turn --"
+        puts "--> #{game_board.current_user.name}'s turn <--"
         say "Choose one number (1 ~ 9):"
 
-        until !game_board.ask_player_to_pick
-          say "Invalid Number..."
-          sleep 1
-        end
-
+        game_board.ask_player_to_pick
+  
         result = game_board.game_is_over?
         break if result
       end
@@ -166,7 +247,7 @@ class TicTacToeGame
 
       puts "\n"
       say "Play again? (y/n)"
-      break if gets.chomp.downcase == 'y'
+      break if gets.chomp.downcase == 'n'
     end
   end
 end
