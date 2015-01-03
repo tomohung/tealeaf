d83c7823a78bc76198d0cd9e2fe47c4423d7a43d
Tomo Hung <tomohung@gmail.com>
2015-01-03 14:51:14 +0800

    mkdir Lesson2

---
+++ Lesson2/oo_prs.rb
@@ -0,0 +1,64 @@
+# 2 or more players show their decisions: Paper, Rock, Scissors
+# check who are winner or tie or loser.
+
+class Player
+
+  PRS_SET = {"P" => "Paper", "R" => "Rock", "S" => "Scissors"}
+  attr_reader :name, :decision
+  def initialize(name)
+    @name = name
+    @name = "Guest" if name == "" 
+  end
+
+  def display_decision
+    PRS_SET[decision]
+  end
+
+  def auto_make_decision
+    @decision = PRS_SET.keys.sample
+  end
+
+  def set_decision(decision)
+    return false unless PRS_SET.has_key?(decision)
+    @decision = decision
+  end
+
+  def match_with(player)
+    self_index = PRS_SET.keys.index(decision)
+    player_index = PRS_SET.keys.index(player.decision)
+
+    case 
+    when self_index == player_index
+      "It's a tie."
+    when self_index - player_index == -1 || self_index - player_index == 2
+      "You Win!!"
+    else
+      "Loser..."
+    end
+  end
+end
+
+puts "Enter Your Name:"
+user_name = gets.chomp
+user = Player.new(user_name)
+computer = Player.new("Computer")
+
+begin
+  system "clear"
+  puts "Hi, #{user.name}"
+  puts "Welcome to Game: Paper Rock Scissors"
+  puts "--------------------------"
+
+  puts "Choose one P)Paper, R)Rock, S)Scissors"
+  if !user.set_decision(gets.chomp.upcase)
+    redo
+  end
+
+  computer.auto_make_decision
+
+  puts "#{user.name}: #{user.display_decision} VS Computer: #{computer.display_decision}"
+  puts user.match_with(computer)
+
+  puts "Play again? (y/n)"
+end while gets.chomp.downcase == 'y'
+
